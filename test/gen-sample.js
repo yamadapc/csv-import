19c96112cd6fb6f2f4d4b7a78f7f41b835913adf
yamadapc <tacla.yamada@gmail.com>
2016-01-28 11:02:51 -0200

    Upgrade what we can and leave everything else here

--- test/gen-sample.js
+++ test/gen-sample.js
@@ -1,18 +1,20 @@
 'use strict';
-var Chance = require('chance'),
-    path = require('path'),
-    fs = require('fs');
+if(!module.parent) {
+  var Chance = require('chance'),
+      path = require('path'),
+      fs = require('fs');
 
-var chance = new Chance();
+  var chance = new Chance();
 
-var records = ['name,tel,job'];
-for(var i = 1; i < 40001; i++) {
-  var entry = '';
-  entry += chance.name();
-  entry += ',' + chance.phone();
-  entry += ',' + chance.word();
+  var records = ['name,tel,job'];
+  for(var i = 1; i < 100; i++) {
+    var entry = '';
+    entry += chance.name();
+    entry += ',' + chance.phone();
+    entry += ',' + chance.word();
 
-  records[i] = entry;
+    records[i] = entry;
+  }
+
+  fs.writeFileSync(path.join(__dirname, 'big-sample.csv'), records.join('\n'));
 }
-
-fs.writeFileSync(path.join(__dirname, 'big-sample.csv'), records.join('\n'));
